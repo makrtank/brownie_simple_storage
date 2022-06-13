ef442533432dc6e164326883c10e4bb524a667f8
Mark Bilodeau <mark@Marks-MacBook-Pro-14.local>
2022-06-13 12:48:02 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Tutorial from FreeCodeCamp:
+Solidity, Blockchain, and Smart Contract Course – Beginner to Expert Python Tutorial
+https://github.com/smartcontractkit/full-blockchain-solidity-course-py
+https://www.youtube.com/watch?v=M576WGiDBdQ&t=3s
---
+++ scripts/read_value.py
@@ -0,0 +1,13 @@
+from lib2to3.pgen2.literals import simple_escapes
+from brownie import SimpleStorage, accounts, config
+
+
+def read_contract():
+    simple_storage = SimpleStorage[-1]
+    # ABI
+    # Address
+    print(simple_storage.retrieve())
+
+
+def main():
+    read_contract()
